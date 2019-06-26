f5b77ed1fa991a23fe5d6a32cb74e7993a4f0ed4
thule <lunthu@gmail.com>
2019-06-26 19:25:03 +0700

    rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :countries, only: [:index, :show, :new, :create, :update, :destroy] do
+    resources :locations, only: [:create]
+  end
+  resources :activities, only: [:index, :show, :new, :create, :update, :destroy]
+
+  namespace :providers do
+    resources :bookings, only: [:index, :update, :destroy]
+    resources :tours, only: [:index, :edit, :destroy]
+  end
+
 end
