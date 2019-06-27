9c06b467916fffe7e9604ec6648f03eb97445164
thule <lunthu@gmail.com>
2019-06-27 09:47:46 +0700

    pundit install setting up for country

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,2 +1,59 @@
 class CountriesController < ApplicationController
+  skip_before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @countries = policy_scope(Country).order(created_at: :desc)
+  end
+
+  def show
+    @country = Country.find(params[:id])
+    authorize @country
+  end
+
+  def new
+    @country = Country.new
+    authorize @country
+  end
+
+  def create
+    @country = Country.new(country_params)
+    authorize @country
+    if @country.save
+      flash[:success] = "Category was created successfully"
+      redirect_to countries_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @country = Country.find(params[:id])
+    authorize @country
+  end
+
+  def update
+    country = current_user.categories.find(params[:id])
+    authorize @country
+    if  country.update(country_params)
+        redirect_to countries_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @country = Category.find(params[:id])
+    authorize @country
+    @country.destroy
+    redirect_to countries_path
+  end
+
+  private
+  def category_params
+    params.require(:country).permit(:name, :summary, :essential_info, :getting_there,
+                                    :getting_around, :weather_climate, :visa_permit,
+                                    :local_customs, :festival_events)
+
+  end
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,5 @@ class Location < ApplicationRecord
   belongs_to :country, foreign_key: "country_id"
   has_many :tours, dependent: :delete_all
   validates :name, presence: true
+  validates :address, presence: true
 end
