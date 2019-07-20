664b3086f3054643db969c32684f5946c8718cf6
thule <lunthu@gmail.com>
2019-07-20 12:44:59 +0700

    tours index, search filter work without ajax

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -2,9 +2,10 @@ class ToursController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show, :likes, :unlikes]
 
   def index
-    if params[:query].present?
+    if params[:query].present? || params["search"]
       tours = policy_scope(Tour).order(created_at: :desc)
-      @tours = tours.search_for_tour(params[:query])
+      @filter = params[:query].present? || params["search"]["activity"].concat(params["search"]["location"]).flatten.reject(&:blank?)
+      @tours = tours.search_for_tour(params[:query]) || @filter.empty? ? Tour.all : Tour.all.tagged_with(@filter, any: true)
     else
       @tours = policy_scope(Tour).order(created_at: :desc)
     end
@@ -54,24 +55,30 @@ class ToursController < ApplicationController
     @user = current_or_guest_user # before_action :authenticate_user, only: [:likes]
     set_tour
     @user.like!(@tour)
-    redirect_to tour_path, alert: "Unsaved"
+      respond_to do |format|
+      format.html { redirect_to :back, alert: "Unsaved" }
+      format.js { render layout: false }
+      end
+    #redirect_to tour_path, alert: "Unsaved"
   end
 
   def unlikes
     @user = current_or_guest_user
     set_tour
     @user.unlike!(@tour)
-    redirect_to tour_path, alert: "Save for later"
+    respond_to do |format|
+      format.html { redirect_to :back, alert: "Unsaved" }
+      format.js { render layout: false }
+      end
+    #redirect_to tour_path, alert: "Save for later"
   end
+
+  private
 
   def set_tour
     @tour = Tour.find(params[:id])
     authorize @tour
   end
-
-
-
-  private
 
   def tour_params
     params.require(:tour).permit(:name, :content, :itinerary, :price, :code,
